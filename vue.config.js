9a17fea1f050a66a5ba249b61c77988fcf27f09e
tukasa <497116162@qq.com>
2022-08-12 20:42:48 +0800

    init

---
+++ vue.config.js
@@ -0,0 +1,35 @@
+const path = require("path");
+module.exports = {
+  lintOnSave: false,
+  devServer: {
+    // 反向代理服务器
+    proxy: {
+      // 当我们的本地的请求 有/api的时候，就会代理我们的请求地址向另外一个服务器发出请求
+      "/api": {
+        target: "http://liufusong.top:7001", // 跨域请求的地址
+        changeOrigin: true, // 只有这个值为true的情况下 才表示开启跨域
+      },
+    },
+  },
+  chainWebpack: (config) => {
+    const svgRule = config.module.rule("svg");
+    svgRule.uses.clear();
+    svgRule
+      .test(/.svg$/)
+      .include.add(path.resolve(__dirname, "./src/icons/svg"))
+      .end()
+      .use("svg-sprite-loader")
+      .loader("svg-sprite-loader")
+      .options({
+        symbolId: "icon-[name]",
+      });
+    const fileRule = config.module.rule("file");
+    fileRule.uses.clear();
+    fileRule
+      .test(/.svg$/)
+      .exclude.add(path.resolve(__dirname, "./src/icons/svg"))
+      .end()
+      .use("file-loader")
+      .loader("file-loader");
+  },
+};
